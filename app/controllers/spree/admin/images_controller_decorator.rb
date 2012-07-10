8af821840f5e3e35597ceab944e3f4511aeeae8f
Peter Berkenbosch <peter@pero-ict.nl>
2012-07-10 11:46:26 +0200

    * don't install any css or js in the store all. * make crop actually use the correct aspectRatio. * fixed the preview (needs some extra styling...)

--- app/controllers/spree/admin/images_controller_decorator.rb
+++ app/controllers/spree/admin/images_controller_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Admin::ImagesController.class_eval do
   def crop
-	@product_dimension = Spree::Image.attachment_definitions[:attachment][:styles][:product]
+	 @product_dimension = Spree::Image.attachment_definitions[:attachment][:styles][:product].split("x").map(&:to_i)
   end
 end
--- lib/generators/spree_image_cropper/install/install_generator.rb
+++ lib/generators/spree_image_cropper/install/install_generator.rb
@@ -3,12 +3,10 @@ module SpreeImageCropper
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_image_cropper\n"
         append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_image_cropper\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_image_cropper\n", :before => /\*\//, :verbose => true
         inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_image_cropper\n", :before => /\*\//, :verbose => true
       end
 
